49c4583e90084623c89f10a6ebdd6500352ed964
shiliangl <shiliangwei@cloudolp.com>
2018-01-24 16:42:17 +0800

    init_project

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <p>this is a simple section</p>
+            </div>
+
+        );
+    }
+}
